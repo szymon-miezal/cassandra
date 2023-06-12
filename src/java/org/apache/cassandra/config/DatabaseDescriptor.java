cfe8598ba9cd84eaa549d68bb3f271c04b4fdc3a
Marcus Eriksson <marcus_eriksson@apple.com>
2023-06-12 16:39:58 +0200

    [CEP-21] Disaster recovery
    Co-authored-by: Sam Tunnicliffe <samt@apache.org>
    Co-authored-by: Marcus Eriksson <marcus_eriksson@apple.com>
    Co-authored-by: Alex Petrov <oleksandr.petrov@gmail.com>

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -4974,4 +4974,9 @@ public class DatabaseDescriptor
     {
         conf.reject_out_of_token_range_requests = enabled;
     }
+
+    public static boolean getUnsafeTCMMode()
+    {
+        return conf.unsafe_tcm_mode;
+    }
 }
